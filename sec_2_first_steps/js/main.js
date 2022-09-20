85f5e8035386229bc88e836f1367990e1a597f9e
lou <dustyfingers01257@gmail.com>
2022-09-20 11:28:58 -0600

    built first custom vertex and fragment shaders!

--- sec_2_first_steps/js/main.js
+++ sec_2_first_steps/js/main.js
@@ -1,5 +1,22 @@
 import * as three from 'three';
 
+// shaders
+const vertexShader = `
+    void main() {
+        // try multiplying the position value passed into vec4 to change the scale of the object!
+        // this shader MUST set the value of gl_Position
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position * 0.5, 1.0);
+    }
+`;
+
+const fragmentShader = `
+    void main() {
+        // gl_FragColor is an rgba-format value
+        // this shader MUST set the value of gl_FragColor
+        gl_FragColor = vec4(0.1, 0.4, 0.2, 0.3);
+    }
+`;
+
 const scene = new three.Scene();
 const camera = new three.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
 const renderer = new three.WebGLRenderer();
@@ -8,7 +25,10 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new three.PlaneGeometry(2, 2);
-const material = new three.ShaderMaterial();
+const material = new three.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+});
 const plane = new three.Mesh(geometry, material);
 scene.add(plane);
 
