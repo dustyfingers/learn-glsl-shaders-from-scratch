5e8342f975ed9ddae3cbc81d6c1a9381ea621419
lou <dustyfingers01257@gmail.com>
2022-09-20 11:21:40 -0600

    got plane added to scene!

--- sec_2_first_steps/js/main.js
+++ sec_2_first_steps/js/main.js
@@ -1,3 +1,42 @@
 import * as three from 'three';
 
-console.log(three);
+const scene = new three.Scene();
+const camera = new three.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
+const renderer = new three.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new three.PlaneGeometry(2, 2);
+const material = new three.ShaderMaterial();
+const plane = new three.Mesh(geometry, material);
+scene.add(plane);
+
+camera.position.z = 1;
+
+onWindowResize();
+animate();
+
+function animate() {
+    // actually renders the scene
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+function onWindowResize(event) {
+    const aspectRatio = window.innerWidth / window.innerHeight;
+    let width, height;
+    if (aspectRatio >= 1) {
+        width = 1;
+        height = (window.innerHeight / window.innerWidth) * width;
+    } else {
+        width = aspectRatio;
+        height = 1;
+    }
+    camera.left = -width;
+    camera.right = width;
+    camera.top = height;
+    camera.bottom = -height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
