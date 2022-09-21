5dad830efce50932331b2cd04a7565e4ed4dd6a0
lou <dustyfingers01257@gmail.com>
2022-09-20 20:42:39 -0600

    changed color based on movement of mouse using uniforms

--- sec_2_first_steps/js/main.js
+++ sec_2_first_steps/js/main.js
@@ -5,16 +5,19 @@ const vertexShader = `
     void main() {
         // try multiplying the position value passed into vec4 to change the scale of the object!
         // this shader MUST set the value of gl_Position
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position * 0.5, 1.0);
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }
 `;
 
 const fragmentShader = `
     uniform vec3 u_color;
+    uniform vec2 u_mouse;
+    uniform vec2 u_resolution;
     void main() {
         // gl_FragColor is an rgba-format value
         // this shader MUST set the value of gl_FragColor
-        gl_FragColor = vec4(u_color, 1.0);
+        vec3 color = vec3(u_mouse.x/u_resolution.x, 0.0, u_mouse.y/u_resolution.y);
+        gl_FragColor = vec4(color, 1.0);
     }
 `;
 
@@ -28,6 +31,9 @@ document.body.appendChild(renderer.domElement);
 const geometry = new three.PlaneGeometry(2, 2);
 
 const uniforms = {
+    u_time: { value: 0.0 },
+    u_mouse: { value: { x: 0.0, y: 0.0 } },
+    u_resolution: { value: { x: 0.0, y: 0.0 } },
     u_color: { value: new three.Color(0xff0000) },
 };
 const material = new three.ShaderMaterial({
@@ -41,7 +47,18 @@ scene.add(plane);
 camera.position.z = 1;
 
 onWindowResize();
+if ('ontouchstart' in window) {
+    document.addEventListener('touchmove', move);
+} else {
+    window.addEventListener('resize', onWindowResize, false);
+    document.addEventListener('mousemove', move);
+}
 animate();
+
+function move(evt) {
+    uniforms.u_mouse.value.x = evt.touches ? evt.touches[0].clientX : evt.clientX;
+    uniforms.u_mouse.value.y = evt.touches ? evt.touches[0].clientY : evt.clientY;
+}
 
 function animate() {
     // actually renders the scene
@@ -65,4 +82,8 @@ function onWindowResize(event) {
     camera.bottom = -height;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    if (uniforms.u_resolution !== undefined) {
+        uniforms.u_resolution.value.x = window.innerWidth;
+        uniforms.u_resolution.value.y = window.innerHeight;
+    }
 }
